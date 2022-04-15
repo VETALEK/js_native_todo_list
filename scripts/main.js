37b6756a386095c2cdf436c84a660aeb4d8e6e62
Avdyeyenko Vitalik <dalshe.budet.sobaka@gmail.com>
2022-04-15 19:26:23 +0300

    Full app markup generation implemented

--- scripts/main.js
+++ scripts/main.js
@@ -1,178 +1,5 @@
 'use strict'
 
-function renderTodoList() {
-  todoList.innerHTML = todoObjects.map(obj => `
-    <li
-      class="todo-item ${obj.isCompleted ? 'completed' : ''}"
-      data-id="${obj.id}"
-    >
-      <input
-        type="checkbox"
-        id="todo-${obj.id}"
-        class="toggle"
-        ${obj.isCompleted ? 'checked' : ''}
-      >
-      <label for="todo-${obj.id}">${obj.name}</label>
-      <button class="destroy"></button>
-    </li>
-  `).join('');
-
-  updateTotalToggler();
-  updateClearCompleted();
-  updateItemsLeft();
-  updateFooter();
-}
-
-function updateItemsLeft() {
-  const total = todoList.querySelectorAll('li').length;
-  const done = todoList.querySelectorAll('.completed').length;
-
-  itemsLeftCounter.textContent = total - done;
-}
-
-function addInputedTodo() {
-  const inputText = todoInput.value.trim();
-  
-  if (inputText.length <= 0) {
-    return;
-  }
-
-  todoObjects.push({
-    id: Number(new Date()) % 100_000,
-    name: inputText,
-    isCompleted: false,
-  });
-
-  todoInput.value = '';
-
-  renderTodoList();
-}
-
-function removeTodo(event) {
-  if (!event.target.classList.contains('destroy')) {
-    return;
-  }
-
-  const todo = event.target.closest('.todo-item');
-  const todoIndex = todoObjects.getIndexById(todo.dataset.id);
-
-  todoObjects.splice(todoIndex, 1);
-
-  renderTodoList();
-}
-
-function toggleTodo(event) {
-  const todoEl = event.target.closest('.todo-item');
-  const todoIndex = todoObjects.getIndexById(todoEl.dataset.id);
-  const todoObj = todoObjects[todoIndex];
-
-  todoObj.isCompleted = !todoObj.isCompleted;
-
-  renderTodoList();
-}
-
-function toggleAllTodos() {
-  const isAllCompleted = totalToggler.checked;
-  
-  todoObjects.forEach(obj => obj.isCompleted = isAllCompleted);
-
-  renderTodoList();
-}
-
-function updateTotalToggler() {
-  const todos = todoList.querySelectorAll('.todo-item');
-  const totalTogglerLabel = root.querySelector('.toggle-all-label');
-
-  totalTogglerLabel.hidden = todos.length <= 0;
-
-  totalToggler.checked = [...todos]
-    .every(todo => todo.matches('.completed'));
-}
-
-function updateClearCompleted() {
-  buttonClearCompleted.hidden = todoList.querySelectorAll('.completed').length <= 0;
-}
-
-function updateFooter() {
-  root.querySelector('.footer').hidden = todoList
-    .querySelectorAll('.todo-item').length <= 0;
-}
-
-function showAllTodos() {
-  todoList.querySelectorAll('.todo-item')
-    .forEach(todo => {
-        todo.hidden = false;
-    });
-}
-
-function showActiveTodos() {
-  todoList.querySelectorAll('.todo-item')
-    .forEach(todo => {
-      todo.hidden = todo.matches('.completed');
-    });
-}
-
-function showCompletedTodos() {
-  todoList.querySelectorAll('.todo-item')
-    .forEach(todo => {
-      todo.hidden = !todo.matches('.completed');
-    });
-}
-
-function clearCompletedTodos() {
-  for (let i = 0; i < todoObjects.length; i++) {
-    const currentObj = todoObjects[i];
-    
-    if (currentObj.isCompleted) {
-      todoObjects.splice(i, 1);
-      i--;
-    }
-  }
-
-  renderTodoList();
-}
-
-function markSelectedFilter(event) {
-  filtersList.querySelectorAll('.show-filter')
-    .forEach(filter => filter.classList.remove('selected'));
-  
-  event.target.classList.add('selected');
-}
-
-function applyCurrentFilter() {
-  const filterMode = filtersList.querySelector('.selected');
-
-  switch (filterMode.dataset.filter) {
-    case 'all': {
-      showAllTodos();
-      break;
-    }
-
-    case 'active': {
-      showActiveTodos();
-      break;
-    }
-
-    case 'completed': {
-      showCompletedTodos();
-      break;
-    }
-
-    default: {
-      return;
-    }
-  }
-}
-
-const root = document.querySelector('.todoapp');
-const todoInput = root.querySelector('.new-todo');
-const todoList = root.querySelector('.todo-list');
-const itemsLeftCounter = root.querySelector('.todo-count')
-  .querySelector('strong');
-const totalToggler = root.querySelector('.toggle-all');
-const filtersList = root.querySelector('.filters');
-const buttonClearCompleted = root.querySelector('.clear-completed');
-
 const todoObjects = [
   {
     id: 1,
@@ -191,26 +18,199 @@ todoObjects.getIndexById = function getIndexById(id) {
     .find(({ id: currentId }) => currentId === +id));
 };
 
-renderTodoList();
+function renderTodoApp() {
+  const activeTodos = todoObjects
+    .filter(({ isCompleted }) => !isCompleted).length;
 
-root.addEventListener('click', applyCurrentFilter);
-root.addEventListener('keypress', (event) => {
-    if (event.key === 'Enter') {
-      applyCurrentFilter();
+  const header = `
+    <header class="header">
+      <h1>todos</h1>
+      <input
+        class="new-todo"
+        placeholder="What needs to be done?"
+        autofocus=""
+        onkeydown="tryAddTodo(event)"
+      >
+    </header>
+  `;
+  const main = `
+    <section class="main">
+      <input
+        id="toggle-all"
+        class="toggle-all"
+        type="checkbox"
+        ${activeTodos <= 0 ? 'checked' : ''}
+        onchange="setAllCompletedTo(event.target.checked)"
+      >
+      <label
+        for="toggle-all"
+        class="toggle-all-label"
+      >
+        Mark all as complete
+      </label>
+
+      <ul class="todo-list">
+
+      ${
+        todoObjects.map(obj => {
+          if (filter === 'completed' && obj.isCompleted
+            || filter === 'active' && !obj.isCompleted
+            || filter === 'all') {
+            return `
+              <li
+                class="todo-item ${obj.isCompleted ? 'completed' : ''}"
+                data-id="${obj.id}"
+              >
+                <input
+                  type="checkbox"
+                  id="todo-${obj.id}"
+                  class="toggle"
+                  ${obj.isCompleted ? 'checked' : ''}
+                  onclick="toggleTodo(${obj.id})"
+                >
+                <label
+                  for="todo-${obj.id}"
+                  onclick="toggleTodo(${obj.id})"
+                >
+                  ${obj.name}
+                </label>
+                <button
+                  class="destroy"
+                  onclick="removeTodo(${obj.id})"
+                ></button>
+              </li>
+            `;
+          }
+        }).join('')
+      }
+
+      </ul>
+    </section>
+  `;
+  const footer = `
+    <footer class="footer">
+      <span class="todo-count">
+        ${activeTodos} items left
+      </span>
+
+      <ul
+        class="filters"
+        onClick="applyFilter(event.target.dataset.filter)"
+      >
+        <li>
+          <a
+            href="#/"
+            data-filter="all"
+            ${filter === 'all'? 'class="selected"' : ''}"
+          >All</a>
+        </li>
+        <li>
+          <a
+            href="#/active"
+            data-filter="active"
+            ${filter === 'active'? 'class="selected"' : ''}"
+          >Active</a>
+        </li>
+        <li>
+          <a
+            href="#/completed"
+            data-filter="completed"
+            ${filter === 'completed'? 'class="selected"' : ''}"
+          >Completed</a>
+        </li>
+      </ul>
+
+      ${
+        todoObjects.length !== activeTodos 
+          ? `
+            <button
+              class="clear-completed"
+              onClick="clearCompletedTodos()"
+            >
+              Clear completed
+            </button>
+          `
+          : ''
+      }
+    </footer>
+  `;
+
+  root.innerHTML = `
+    ${header}
+
+    ${
+      todoObjects.length <= 0
+        ? ''
+        : `
+          ${main}
+          ${footer}
+          `
     }
+  `;
+}
+
+function tryAddTodo(event) {
+  if (event.key !== 'Enter') {
+    return;
+  }
+
+  const inputText = event.target.value.trim();
+  
+  if (inputText.length <= 0) {
+    return;
+  }
+
+  todoObjects.push({
+    id: Number(new Date()) % 100_000,
+    name: inputText,
+    isCompleted: false,
   });
 
-todoInput.addEventListener('keypress', (event) => {
-  if (event.key === 'Enter') {
-    addInputedTodo();
+  renderTodoApp();
+}
+
+function removeTodo(todoId) {
+  const todoIndex = todoObjects.getIndexById(todoId);
+
+  todoObjects.splice(todoIndex, 1);
+
+  renderTodoApp();
+}
+
+function toggleTodo(todoId) {
+  const todoIndex = todoObjects.getIndexById(todoId);
+  const todoObj = todoObjects[todoIndex];
+
+  todoObj.isCompleted = !todoObj.isCompleted;
+
+  renderTodoApp();
+}
+
+function setAllCompletedTo(value) {
+  todoObjects.forEach(obj => obj.isCompleted = value);
+
+  renderTodoApp();
+}
+
+function clearCompletedTodos() {
+  for (let i = 0; i < todoObjects.length; i++) {
+    if (todoObjects[i].isCompleted) {
+      todoObjects.splice(i, 1);
+      i--;
+    }
   }
-});
 
-todoList.addEventListener('click', toggleTodo);
-todoList.addEventListener('click', removeTodo);
+  renderTodoApp();
+}
 
-totalToggler.addEventListener('click', toggleAllTodos);
+function applyFilter(newFilter) {
+  filter = newFilter;
 
-filtersList.addEventListener('click', markSelectedFilter);
+  renderTodoApp();
+}
 
-buttonClearCompleted.addEventListener('click', clearCompletedTodos);
+const root = document.querySelector('.todoapp');
+
+let filter = 'all';
+
+renderTodoApp();
